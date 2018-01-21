369851d291ac081cab81094791124f18aeeaa8f1
rollingw <spsweet123@gmail.com>
2018-01-21 14:10:33 +0000

    remote_file and cookbook_file

--- recipes/server.rb
+++ recipes/server.rb
@@ -1,8 +1,18 @@
 package 'httpd' 
-template '/var/www/html/index.html' do
-  source 'index.erb'
-  action :create
+
+remote_file '/var/www/html/google.png' do 
+
+source 'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png' 
 end
+
+template '/var/www/html/index.html' do
+ source 'index.erb'
+ action :create
+end
+
+#cookbook_file '/var/www/html/index.html' do
+#  source 'index.html'
+#end
 
 service 'httpd' do
   action [:enable, :start]
